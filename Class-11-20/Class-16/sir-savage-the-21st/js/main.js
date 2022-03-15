ec4a58d8f4ff1e936fe55a6ccc1544fcb4876a1a
Hachikoi-the-creator <facka72@gmail.com>
2022-03-15 13:21:48 -0600

    hw finished

--- Class-11-20/Class-16/sir-savage-the-21st/js/main.js
+++ Class-11-20/Class-16/sir-savage-the-21st/js/main.js
@@ -1,2 +1,11 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+const placeHoldinger = document.getElementById("savage-says");
+
+for (let index = 0; index < 21; index++) {
+  if (placeHoldinger) {
+    placeHoldinger.textContent += "21 ";
+  } else {
+    placeHoldinger.textContent = "21 ";
+  }
+}
