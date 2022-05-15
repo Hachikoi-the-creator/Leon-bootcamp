8d82dcde6bd11508cfc88dea75e3e2ac52786f39
Hachikoi-the-creator <facka72@gmail.com>
2022-05-15 15:04:35 -0500

    got 1 failed 1

--- Codewars/compilation4.js
+++ Codewars/compilation4.js
@@ -16,4 +16,57 @@ function fakeBin(x) {
 }
 
 // 6 - Cyclist drinks 0.5 every 1 meter, round it down
-const litres = (time) => Math.floor(time / 2); 
+const litres = (time) => Math.floor(time / 2);
+
+// 7 - Return if the constumer is senior or not
+/*
+*@params data = list of list
+*@params data[0] = age
+*@params data[1] = handicap
+*/
+function openOrSenior(data) {
+  // Senior : age >= 55 && handicap > 
+  const res = data.map(subList => {
+    if (subList[0] >= 55 && subList[1] > 7) {
+      return 'Senior';
+    }
+    else {
+      return 'Open';
+    }
+  });
+
+  return res;
+  // const pop = [[59, 12], [55, -1], [12, -2], [12, 12]];
+}
+
+//! BETTER ANSWER
+// Destructuring: [age, handicap] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+
+function openOrSenior(data) {
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
+
+
+// 8x - Check if arr b has the squares of arr a, in any order
+// Both will always have the same len
+
+function comp(array1, array2) {
+  if (array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b);
+  array2.sort((a, b) => a - b);
+  return array1.map(v => v * v).every((v, i) => v == array2[i]);
+  // a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+  // a2 = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19];
+}
+
+// ? whyt this not working?
+function coompare(array1, array2) {
+  let check = true;
+  array1.forEach(e => array2.includes(e * e) ? '' : check = false);
+  return check;
+}
+
+
+// 9 -
+
+// 10 -
