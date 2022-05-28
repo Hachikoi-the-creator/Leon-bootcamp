128a8b277c0837e68bd47478bd10db7e5506cfff
Hachikoi-the-creator <facka72@gmail.com>
2022-05-28 14:01:14 -0500

    added 2 more

--- Codewars/compilation6.js
+++ Codewars/compilation6.js
@@ -2,3 +2,15 @@
 var isSquare = function (n) {
     return Math.sqrt(n) % 1 == 0;
 };
+
+// 2 - easy dubs
+function find_average(array = []) {
+    if (array.length == [].length) return 0;
+
+    return array.reduce((a, b) => a + b) / array.length;
+}
+
+// 3 - given 2 word space separeted, return the initials of the person
+function abbrevName(name = "") {
+    return name.split(' ').map(word => word[0].toUpperCase()).join('.');
+}
