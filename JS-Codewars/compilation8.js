4d5e19118d2a3bb554cd8861648e7d054d869503
Hachikoi-the-creator <facka72@gmail.com>
2022-07-22 14:09:24 -0500

    another

--- JS-Codewars/compilation8.js
+++ JS-Codewars/compilation8.js
@@ -6,31 +6,41 @@ const number = (busStops = [[]]) => {
     console.log("plus ", arr[0], "less: ", arr[1]);
     res += arr[0];
     res -= arr[1];
-  };
+  }
 
   return res;
 };
 
-
 // 2 - get middle string if "one"-> "n"; "four"-> "ou"
 function getMiddle(str = "") {
   if (str.length % 2 === 0) {
-    return (str[str.length / 2 - 1] + str[str.length / 2]);
+    return str[str.length / 2 - 1] + str[str.length / 2];
   } else {
-    return (str[Math.floor(str.length / 2)]);
+    return str[Math.floor(str.length / 2)];
   }
 }
 
 //3 - Return the string separated in pairs, if the str is not pair, add an _ at the end
-function solution(str){
-  if(str.length % 2 !== 0) str +="_"
-  console.log(str)
-  
-  let res = []
+function solution(str) {
+  if (str.length % 2 !== 0) str += "_";
+  console.log(str);
 
-  for (let i = 0; i < str.length; i+=2) {
-    res.push(str[i]+str[i+1])
+  let res = [];
+
+  for (let i = 0; i < str.length; i += 2) {
+    res.push(str[i] + str[i + 1]);
   }
 
-  return res
+  return res;
+}
+
+// 4 - given 2 arrays, remove every occurence of arrA found in arrB
+// ?A [1,2,2,2,3]
+// ?B [2]
+// ![1,3]
+function arrayDiff(arrA = [], arrB = []) {
+  const res = arrA.filter((el) => {
+    return arrB.every((e) => e !== el);
+  });
+  return res;
 }
